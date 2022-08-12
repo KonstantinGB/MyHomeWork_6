0b00f754d3834e5dcef74fe5c02cf85f7a9af447
Konstantin <osipo38@yandex.ru>
2022-08-12 11:35:41 +0300

    save

---
+++ Exercise6_1/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+string Bolshenyl(int a) 
+{
+    int sum = 0;
+    int temp = 0;
+    string stoka =string.Empty;
+    string stoooka = "Чисел больше 0: ";
+    for(int i=0; i<a; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        temp = Convert.ToInt32(Console.ReadLine());
+        stoka += temp;
+
+        if(temp > 0) sum += 1;
+    }
+    stoooka += sum;
+    return stoooka;
+}
+
+Console.Write(" Введите какое количество чисел нужно проверить являются ли они больше 0:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string schetchik = Bolshenyl(size);
+Console.Write(schetchik);
---
+++ Exercise6_2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+*/
+
+
+string Peresechenie(double k1, double b1, double k2, double b2)
+
+{
+    string koordinata = "Точка пересечения двух прямых:  ";
+    double x = (b2 - b1) / (k1 - k2);
+    double y= k2 * x + b2;
+    koordinata = koordinata + x + "," + y;
+    return koordinata;
+
+}
+
+Console.WriteLine(" Для нахождения точки пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; ");
+Console.Write(" Введите значение k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write(" Введите значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write(" Введите значение k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write(" Введите значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+string koordinata = Peresechenie(k1, b1, k2, b2);
+Console.Write(koordinata);
